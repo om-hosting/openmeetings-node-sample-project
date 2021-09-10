b8c6ccce072a66b25dd408ba2275d740bdab9438
Sebastian Wagner <seba.wagner@gmail.com>
2021-09-10 15:33:27 +1200

    Update proejct files.

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+import {UserServiceApi, Configuration} from "openmeetings-node-client"
+
+const config: Configuration = new Configuration({
+    basePath: "http://localhost:5080/openmeetings/services"
+})
+
+app.get('/', async (req, res) => {
+    const userService: UserServiceApi = new UserServiceApi(config);
+    const { data } = await userService.login("admin", "!HansHans")
+    res.send('Login result: ' + data + JSON.stringify(data))
 })
 
 app.listen(port, () => {
